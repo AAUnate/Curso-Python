bd3f86535952ce1918e0bf231ef1c5bf56226f21
Alejandro Ariel Uñate <unatealejandroariel@gmail.com>
2026-01-11 22:03:28 -0300

    primer modificado

--- python/primer.py
+++ python/primer.py
@@ -1,1 +1,7 @@
-print("hola")
+import os
+os.system("cls" if os.name == "nt" else "clear")
+nombre = input("Ingrese nombre:  ")
+apellido = input("ingrese apellido:  ")
+print(f"hola nombre {nombre} de apellido {apellido}")
+
+print('Adios, hasta la proxima Ariel, suerte Uñate')
