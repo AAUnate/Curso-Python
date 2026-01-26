c172c8e09d55f91fb7fb49a9fa0a546bccbae0b0
Alejandro Ariel Uñate <unatealejandroariel@gmail.com>
2026-01-25 22:15:46 -0300

    commit sin add

--- python/primer.py
+++ python/primer.py
@@ -1,7 +1,16 @@
 import os
 os.system("cls" if os.name == "nt" else "clear")
-nombre = input("Ingrese nombre:  ")
-apellido = input("ingrese apellido:  ")
-print(f"hola nombre {nombre} de apellido {apellido}")
-
+seguir = "si"
+while (seguir == "si"):
+    nombre = input("Ingrese nombre:  ")
+    apellido = input("ingrese apellido:  ")
+    print(f"hola nombre {nombre} de apellido {apellido}")
+    seguir = input("Desea continuar S/N: ")
+    verificar = "si" if (seguir == "si") else "si" if (seguir == "s") else "si" if (seguir == "SI") else "si" if (seguir == "S") else "no" 
+    if verificar == "si":
+        seguir="si"
+        continue
+    else:
+        seguir="no"
+        break
 print('Adios, hasta la proxima Ariel, suerte Uñate')
