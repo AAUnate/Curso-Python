d86e16ec192c6343c736a61136fbff7e299750cc
Alejandro Ariel Uñate <unatealejandroariel@gmail.com>
2026-02-07 17:42:13 -0300

    arregle temporal

---
+++ bash/matriz.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+mostrar(){
+    echo "${matriz[0]} ${matriz[1]} ${matriz[2]} ${matriz[3]}"
+    echo "$i"
+    j=0
+    while [[ $j < $i ]]; do
+        echo "matriz-Nombre: ${matriz[$j]}"
+        j=$(( 1+j ))
+        echo "Matriz-edad: ${matriz[$j]}"
+        j=$(( 1+j ))
+    done
+}
+
+clear
+seguir="S"
+i=0
+while [[ "$seguir" =~ ^[Ss]$ ]]; do
+    read -p "Ingrese El Nombre: " matriz[$i]
+    i=$(( 1+i ))
+    read -p "Ingrese La Edad: " matriz[$i]
+    #echo "matriz ${matriz[0]} ${matriz[1]}"
+    i=$(( 1+i ))
+    read -p "Desea continuar: " seguir
+    if ! [[ "$seguir" =~ ^[Ss]$ ]]; then
+        echo "adios..."
+        
+        mostrar $matriz $i
+        exit 1
+    else
+        continue
+    fi
+done
---
+++ javascript/mostrar.js
@@ -0,0 +1,9 @@
+console.clear();
+let prompt = require("prompt-sync")();
+let nombre = prompt("ingrese su nombre: ")
+seguir = "S";
+palabra = "S";
+while (seguir == "S"){
+    console.log("final putaso " + nombre)
+    break
+}
